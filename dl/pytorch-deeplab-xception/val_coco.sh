cb31b504f490f81d3d0a6cff30e23724e084a2f9
befreor <befreor@gmail.com>
2019-09-02 13:36:14 +0900

    deeplab

---
+++ dl/pytorch-deeplab-xception/val_coco.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1 python val.py --backbone resnet --lr 0.01 --workers 1 --epochs 40 --batch-size 2 --gpu-ids 0,1 --checkname deeplab-resnet --eval-interval 1 --dataset coco --resume=run/coco/deeplab-resnet/experiment_1/checkpoint.pth.tar
